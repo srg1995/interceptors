59cff85e741ef6985aee0fdc1f4800d7b756a874
Sergio Sacristan <sergiosacristan8@gmail.com>
2025-10-03 09:49:43 +0200

    refresh token service config

---
+++ src/app/services/auth-service.ts
@@ -0,0 +1,34 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, throwError, map, tap, catchError } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  baseURL = 'http://localhost:4200';
+  http = inject(HttpClient);
+
+  //el token nosotros lo utilizamos para las peticiones(autenticacion), para la autorizacion seria tema de roles
+  refreshToken(): Observable<string> {
+    const refreshToken = localStorage.getItem('refreshToken');
+    if (!refreshToken) {
+      this.logOut();
+      return throwError(() => new Error('No refresh token available'));
+    }
+    return this.http
+      .post<{ refresToken: string }>(`${this.baseURL}/refresh-token`, { refreshToken })
+      .pipe(
+        map((response) => response.refresToken),
+        tap((newAccessToken: string) => {
+          localStorage.setItem('token', newAccessToken);
+        }),
+        catchError((error) => {
+          this.logOut();
+          return throwError(() => new Error('Failed to refresh token'));
+        })
+      );
+  }
+
+  logOut() {}
+}
